b95cacf4b072988e6157b9ee03ae0666e53e8c66
Pearu Peterson <pearu.peterson@gmail.com>
2018-12-14 01:36:02 +0200

    Fixing appveyor errors.

--- c/umem.hpp
+++ c/umem.hpp
@@ -74,16 +74,16 @@ namespace umem {
   private:
     uintptr_t adr;
     alignment_t alignment;
-    size_t nbytes;
+    size_t size;
     void * raw_ctx;
     bool own;
   public:
-    Address(uintptr_t adr, alignment_t alignment,  size_t nbytes, void * raw_ctx, bool own = false):
-      adr(adr), alignment(alignment), nbytes(nbytes), raw_ctx(raw_ctx), own(own) {
-      std::cout << "  Address::Address("<<adr<<", "<<alignment<<", "<<nbytes<<", ctx="<<raw_ctx<<", own="<<own<<")\n";
+    Address(uintptr_t adr, alignment_t alignment,  size_t size, void * raw_ctx, bool own = false):
+      adr(adr), alignment(alignment), size(size), raw_ctx(raw_ctx), own(own) {
+      std::cout << "  Address::Address("<<adr<<", "<<alignment<<", "<<size<<", ctx="<<raw_ctx<<", own="<<own<<")\n";
     }
     ~Address() {
-      std::cout << "  Address::~Address()["<<adr<<", "<<alignment<<", "<<nbytes<<", ctx="<<raw_ctx<<", own="<<own<<"]\n";
+      std::cout << "  Address::~Address()["<<adr<<", "<<alignment<<", "<<size<<", ctx="<<raw_ctx<<", own="<<own<<"]\n";
       if (own) {
         if (alignment)
           umem_aligned_free(raw_ctx, adr);
@@ -95,15 +95,15 @@ namespace umem {
     inline void copy_to(Address& dest, size_t nbytes) { umem_copy_to(raw_ctx, adr, dest.get_raw_context(), (uintptr_t)dest, nbytes); }
     inline void copy_from(Address& src, size_t nbytes) { umem_copy_from(raw_ctx, adr, src.get_raw_context(), (uintptr_t)src, nbytes); }
     inline void copy_from(std::string src, size_t nbytes) { umem_copy_from(raw_ctx, adr, ((umemVirtual*)raw_ctx)->host, (uintptr_t)src.c_str(), nbytes); }
-    inline Address connect(Context& dest, size_t dest_nbytes, size_t dest_alignment = 0) {
+    inline Address connect(Context& dest, size_t dest_nbytes, alignment_t dest_alignment = 0) {
       return Address(umem_connect(raw_ctx, adr, dest_nbytes, dest.get_raw_context(), dest_alignment), dest_alignment, dest_nbytes, dest.get_raw_context());
     }
     inline void disconnect(Address& dest) { umem_disconnect(raw_ctx, adr, dest.get_raw_context(), (uintptr_t)dest, (alignment_t)dest); }
     inline void sync_to(Address& dest, size_t nbytes) { umem_sync_to(raw_ctx, adr, dest.get_raw_context(), (uintptr_t)dest, nbytes); }
     inline void sync_from(Address& src, size_t nbytes) { umem_sync_from(raw_ctx, adr, src.get_raw_context(), (uintptr_t)src, nbytes); }
     
-    inline char& operator[] (size_t n) { return ((char*)adr)[n]; }
-    inline void set(int c, size_t nbytes) { umem_set(raw_ctx, adr, c, nbytes); };
+    inline char& operator[] (size_t n) { return ((char*)adr)[n]; } // TODO: ensure n<size
+    inline void set(int c, size_t nbytes) { umem_set(raw_ctx, adr, c, nbytes); }; // TODO: ensure nbytes<size
 
     inline operator uintptr_t() { return adr; }
     inline operator alignment_t() { return alignment; }
@@ -142,13 +142,13 @@ namespace umem {
     inline bool operator == (int i) const { return adr == (unsigned int)i; }
     //inline bool operator == (size_t i) const { return adr == i; }
     //inline bool operator != (uintptr_t i) const { return adr != i; }
-    inline Address operator + (int i) const { return Address(adr + i, alignment, (i<(int)nbytes ? nbytes - i : 0), raw_ctx); }
-    inline Address operator + (size_t i) const { return Address(adr + i, alignment, (i<nbytes ? nbytes - i : 0), raw_ctx); }
-    //inline Address operator - (int i) const { return Address(adr - i, alignment, nbytes + i, raw_ctx); }
-    //inline Address operator - (size_t i) const { return Address(adr - i, alignment, nbytes + i, raw_ctx); }
+    inline Address operator + (int i) const { return Address(adr + i, alignment, (i<(int)size ? size - i : 0), raw_ctx); }
+    inline Address operator + (size_t i) const { return Address(adr + i, alignment, (i<size ? size - i : 0), raw_ctx); }
+    //inline Address operator - (int i) const { return Address(adr - i, alignment, size + i, raw_ctx); }
+    //inline Address operator - (size_t i) const { return Address(adr - i, alignment, size + i, raw_ctx); }
     inline int operator % (int i) const { return adr % i; }
     inline size_t operator % (size_t i) const { return adr % i; }
-    inline Address origin() const { return (alignment ? Address(umem_aligned_origin(raw_ctx, adr), 0, nbytes, raw_ctx) : Address(adr, alignment, nbytes, raw_ctx)); }
+    inline Address origin() const { return (alignment ? Address(umem_aligned_origin(raw_ctx, adr), 0, size, raw_ctx) : Address(adr, alignment, size, raw_ctx)); }
     
   protected:
     inline void * const get_raw_context() { return raw_ctx; };
